61383e667953fbd04f37a92456ebc5e3ba15119f
Patrick Nguyen <patar.nguyen@gmail.com>
2022-04-06 19:25:27 -0400

    Add click

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,8 +7,17 @@ const img_api = "https://image.tmdb.org/t/p/w500";
 
 const { title, poster_path, overview, vote_average } = props;
 
+const rickRolled = () => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+}
+
+const handleClick = (e) => {
+  e.preventDefault();
+  rickRolled();
+}
+
   return(
-    <div className="movie">
+    <div className="movie" onClick={handleClick}>
       <img src={poster_path ? img_api + poster_path : "https://media.istockphoto.com/vectors/image-unavailable-icon-vector-id1206575314?k=20&m=1206575314&s=612x612&w=0&h=vHGhGdWirBbzLm-O15AQuZPnazpHZjtt3vtCBDl-T7g="} alt={title} />
       <div className="movie-info">
         <h3>{title}</h3>
