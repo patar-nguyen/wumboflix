5b37e44827fd8dbf4a37d23c87158e7251b65b0b
Patrick Nguyen <patar.nguyen@gmail.com>
2022-04-06 18:30:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Movie from './components/Movie';
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  const featured_api = `http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1`;
+  const search_api = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=`;
+
+  const [movies, setMovies] = useState([]);
+
+  const [search, setSearch] = useState('');
+
+  useEffect (() => {
+    axios.get(featured_api)
+    .then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+
+  const handleSubmit = (event) => {
+    //prevent page from refreshing
+    event.preventDefault();
+    if (search) {
+      axios.get(search_api + search)
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+    }
+    setSearch('');
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header> 
+      <nav>
+        <div><a className="logo" href="/">Wumboflix</a></div>
+        <ul className="links">
+          <li><a href="/">Home</a></li>
+          <li><form onSubmit={handleSubmit}><input className="search" type="search" placeholder="Search..." value={search} onChange={(e) => {setSearch(e.target.value)}}/></form></li>
+        </ul>
+      </nav>
       </header>
-    </div>
+      <div className="movie-container">  
+        {movies.map((movie => <Movie key={movie.id} {...movie} />))}
+      </div>
+    </>
+
   );
 }
 
---
+++ src/components/Movie.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+export default function Movie(props) {
+
+const img_api = "https://image.tmdb.org/t/p/w500";
+
+const { title, poster_path, overview, vote_average } = props;
+
+  return(
+    <div className="movie">
+      <img src={poster_path ? img_api + poster_path : "https://media.istockphoto.com/vectors/image-unavailable-icon-vector-id1206575314?k=20&m=1206575314&s=612x612&w=0&h=vHGhGdWirBbzLm-O15AQuZPnazpHZjtt3vtCBDl-T7g="} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span className="average">{vote_average}</span>
+      </div>
+      <div className="movie-hover">
+        <h2>Overview: </h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
+}
